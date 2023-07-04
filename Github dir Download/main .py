430bc01fc047e1205be5f245eadcd4438bbe1a4c
Piyush <piyushkr172@gmail.com>
2023-07-04 16:39:02 +0530

    added github directory or file downloader script

---
+++ Github dir Download/main .py
@@ -0,0 +1,35 @@
+import os
+import urllib.request
+
+ft_choice = int(input(
+    """Enter 1 to download a raw File \n Enter 2 to download a whole directory\n ENTER: """))
+
+
+def rawFile():
+    file_path = input("Paste the file URL: ")
+    try:
+        name = file_path.split("/")[-1]
+        urllib.request.urlretrieve(file_path, filename=name)
+        print("Downloaded Successfully")
+    except Exception:
+        print("Download Failed Error occurred")
+        print(Exception.__str__)
+
+
+def directoryDownload():
+    fldr_path = input("Paste the folder URL: ")
+    try:
+        os.system('gitdir ' + fldr_path)
+        print("Downloaded Successfully")
+    except Exception:
+        print("Download Failed Error occurred")
+        print(Exception.__str__)
+
+
+match ft_choice:
+    case 1:
+        rawFile()
+    case 2:
+        directoryDownload()
+    case default:
+        print("Wrong Input")
